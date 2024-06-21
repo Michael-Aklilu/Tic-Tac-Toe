8e6cf3795af95c4628dc1407019659c3189e3692
Michael Aklilu <michaelaklilu@gmail.com>
2024-06-21 10:27:31 +0300

    Add gameboard object

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const gameBoard = function (){
+    const row = 3;
+    const column = 3;
+    let board = [];
+
+    for(let i = 0; i<row;i++){
+        board[i] = [];
+        for(let j = 0; j<column;j++){
+            board.push(" ");
+        }
+    }
+
+    const displayBoard = () => {
+         
+    }
+}
